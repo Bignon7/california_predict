728afcff81e0152f917b47ca43eada4511f740b1
Bignon7 <hindemelais@gmail.com>
2026-03-09 18:36:23 +0100

    My first prediction is workiiinnnnnng

--- prediction_page.py
+++ prediction_page.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import joblib as jb
 
-model = jb.load("data/best_forest_model.pkl")
-scaler = jb.load("data/scaler.pkl")
+model = jb.load("model/best_forest_model.pkl")
+scaler = jb.load("model/scaler.pkl")
 
 prediction = 150
 choices = pd.read_csv("data/housing.csv")
@@ -12,23 +12,21 @@ def show_prediction_page():
     st.title("Californian houses price prediction")
     st.write("Please select your house features")
 
+    col1, col2 = st.columns(2)
 
-    longitude = st.number_input("Longitude", min_value=-125.0, max_value=125.0, value=0.0, step=0.1)
+    with col1:
+        longitude = st.number_input("Longitude", min_value=-125.0, max_value=125.0, value=0.0, step=0.1)
+        housing_median_age = st.number_input("Housing Median Age", min_value=0.0, value=0.0, step=0.1)
+        total_rooms = st.number_input("Total rooms", min_value=0, value=0, step=1)
+        population = st.number_input("Population", min_value=0.0, value=0.0, step=0.1)
+        median_income = st.number_input("Median income", min_value=0.0, value=0.0, step=0.1)
 
-    latitude = st.number_input("Latitude", min_value=-125.0, max_value=125.0, value=0.0, step=0.1)
+    with col2:
+        latitude = st.number_input("Latitude", min_value=-125.0, max_value=125.0, value=0.0, step=0.1)
+        total_bedrooms = st.number_input("Total bedrooms", min_value=0, value=0, step=1)
+        households = st.number_input("Households", min_value=0.0, value=0.0, step=0.1)
+        ocean_proximity = st.selectbox("Ocean Proximity", choices["ocean_proximity"].unique())
 
-    housing_median_age = st.number_input("Housing Median Age", min_value=-125.0, value=0.0, step=0.1)
-
-    total_rooms = st.number_input("Total rooms", min_value=0, value=0, step=1)
-
-    total_bedrooms = st.number_input("Total bedrooms", min_value=0, value=0, step=1)
-
-    population = st.number_input("Population", min_value=0.0, value=0.0, step=0.1)
-
-    households = st.number_input("Households", min_value=0.0, value=0.0, step=0.1)
-
-    ocean_proximity = st.selectbox("Ocean Proximity", choices["ocean_proximity"].unique())
-    
 
     launch_prediction = st.button ("Predict" )
 
@@ -36,6 +34,7 @@ def show_prediction_page():
 
         data = {
             "longitude": longitude,
+            "median_income": median_income,
             "housing_median_age": housing_median_age,   
             "total_rooms": total_rooms,
             "total_bedrooms": total_bedrooms,
@@ -50,7 +49,10 @@ def show_prediction_page():
         data_df["bedroom_ratio"] = data_df["total_bedrooms"] / data_df["total_rooms"]
         data_df["household_ratio"] = data_df["total_rooms"] / data_df["households"]
 
-        data_df.join(pd.get_dummies(data_df["ocean_proximity"], dtype=int)).drop("ocean_proximity", axis=1)
+        data_df = data_df.join(pd.get_dummies(data_df["ocean_proximity"], dtype=int)).drop("ocean_proximity", axis=1)
+        
+        #J'ai ajouté cette ligne pour refaire les colonnes de mon data_df en fonctions des colonnes de mon scaler, avec feature_names_in_
+        data_df = data_df.reindex(columns=scaler.feature_names_in_, fill_value=0)
 
         data_df_s = scaler.transform(data_df)
 
